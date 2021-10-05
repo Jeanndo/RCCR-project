33d91289fca93b68e7389ed78bcdcb08970bcbf8
Jeanndo <jeanndo@pop-os.localdomain>
2021-10-05 20:33:25 +0200

    completely updated rccr to react

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import Navigation from '../Navigation/Navigation'
-import SlideShow  from '../SlideShow/SlideShow'
-import Mission from  '../Mission/Mission'
-import Community from '../Community/Community'
-import Testmonials from '../Testmonials/Testmonials'
-import Signup from '../ChritianLife/ChritianLife'
-import Team from '../Team/Team'
-import Contactus from '../Contactus/Contactus'
-import Footer from '../Footer/Footer'
+import React from "react";
+import Navigation from "../Navigation/Navigation";
+import SlideShow from "../SlideShow/SlideShow";
+import Mission from "../Mission/Mission";
+import Community from "../Community/Community";
+import Testmonials from "../Testmonials/Testmonials";
+import Signup from "../ChritianLife/ChritianLife";
+import Team from "../Team/Team";
+import Contactus from "../Contactus/Contactus";
+import Footer from "../Footer/Footer";
 
 const Landing = () => {
-    return (
-        <div>
-           <Navigation/>
-           <SlideShow/>
-           <Mission/>
-           <Community/>
-           <Testmonials/>
-           <Signup/>
-           <Team/>
-           <Contactus/>
-           <Footer/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navigation />
+      <SlideShow />
+      <Mission />
+      <Community />
+      <Testmonials />
+      <Signup />
+      <Team />
+      <Contactus />
+      <Footer />
+    </div>
+  );
+};
 
-export default Landing
+export default Landing;
