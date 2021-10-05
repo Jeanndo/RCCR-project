b0bc44f8b6ce58cc2000a8a4f3d6a6fd36fb1ef7
Jeanndo <jeanndo@pop-os.localdomain>
2021-10-05 17:54:49 +0200

    created welcome component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import "antd/dist/antd.css";
 import './ui.css'
 import './Mediaqueries.css'
+import './grid.css'
 const App =()=>{
   return (
     <BrowserRouter>
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navigation from '../Navigation/Navigation'
 import SlideShow  from '../SlideShow/SlideShow'
+import Mission from  '../Mission/Mission'
+
 const Landing = () => {
     return (
         <div>
            <Navigation/>
            <SlideShow/>
+           <Mission/>
         </div>
     )
 }
