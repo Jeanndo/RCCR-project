1c0d0e487e80d905615f54f07de5f885959bcbcc
Jeanndo <jeanndo@pop-os.localdomain>
2021-10-08 16:14:33 +0200

    created new componets

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 import Landing from "../components/Landing/Landing"
+import News from '../components/News/News'
 
 class Routes extends React.Component {
     render() {
@@ -8,6 +9,7 @@ class Routes extends React.Component {
         <Router>
           <Switch>
            <Route exact path="/" component={Landing}/>
+           <Route exact path="/news" component={News}/>
           </Switch>
         </Router>
       );
